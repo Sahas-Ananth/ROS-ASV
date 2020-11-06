11b36c41e4410250b8e775ddb1ec312caa94e85d
Sahas-Ananth <sahas_ananth@outlook.com>
2020-11-06 20:10:07 +0530

    Added URDF of Robot and other control packages.
    * ros_asv_description was added which contans robot URDF model and
    Gazebo model.
    * ros_asv_control was added to control the robot with scripts to
    calculate and publish odom from wheel velocities published by arduino.
    * Launch files were added to open gmapping and convert depthimage to
    laserscan.
    * ros_asv_params was added to keep trach of all config files and
    parameter yaml files for ease of use.

---
+++ ros_asv_control/scripts/odom_tf_pub.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import time
+import rospy
+import tf
+import tf_conversions
+from math import cos, sin, pi
+
+from geometry_msgs.msg import Vector3Stamped, Twist, Point, Pose, Quaternion, Vector3
+from nav_msgs.msg import Odometry
+
+WHEEL_BASE = 0.48
+
+
+class Odometry_Publisher():
+    def __init__(self):
+        rospy.init_node("Odometry_Publisher")
+        self.left_vel = 0
+        self.right_vel = 0
+        self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=10)
+        self.odom_broadcastor = tf.TransformBroadcaster()
+        rospy.Subscriber("speed", Vector3Stamped, callback=self.get_wheel_vel)
+
+    def get_wheel_vel(self, vel):
+        self.left_vel = vel.vector.x
+        self.right_vel = vel.vector.y
+
+    def publish_odom(self):
+        x = 0.0
+        y = 0.0
+        th = 0.0
+
+        rate = rospy.Rate(1)
+        last_time = rospy.Time.now()
+        while not rospy.is_shutdown():
+            current_time = rospy.Time.now()
+
+            dt = (current_time - last_time).to_sec()
+
+            vx = (self.left_vel + self.right_vel)/2
+            vy = 0
+            vth = (self.left_vel - self.right_vel)/WHEEL_BASE
+
+            dx = (vx * cos(th))*dt
+            dy = (vy * sin(th))*dt
+            dth = vth*dt
+
+            x += dx
+            y += dy
+            th += dth
+
+            odom_quat = tf_conversions.transformations.quaternion_from_euler(
+                0, 0, th)
+
+            self.odom_broadcastor.sendTransform(
+                (x, y, 0.), odom_quat, current_time, "base_link", "odom")
+
+            odom = Odometry()
+            odom.header.stamp = current_time
+            odom.header.frame_id = "odom"
+
+            odom.pose.pose = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
+
+            odom.child_frame_id = "base_link"
+            odom.twist.twist = Twist(Vector3(vx, vy, 0), Vector3(0, 0, vth))
+
+            self.odom_pub.publish(odom)
+
+            last_time = current_time
+            rate.sleep()
+
+
+if __name__ == "__main__":
+    try:
+        node = Odometry_Publisher()
+        node.publish_odom()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Killing Odom Publisher")
